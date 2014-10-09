8d37897d3991fd70024083e8828aef4dea0766b5
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2014-10-09 12:42:35 +0600

    locale for rails 3 tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,9 @@ require 'coveralls'
 
 Coveralls.wear!
 
+locale_path = Dir.glob(File.dirname(__FILE__) + '/locale/*.yml')
+I18n.load_path += locale_path unless I18n.load_path.include?(locale_path)
+
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
