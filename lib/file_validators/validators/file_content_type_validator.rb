75a15017c7f8d0a60e2a8cb73c282647d7329f21
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2014-10-11 10:16:49 +0600

    refactoring validate list methods

--- lib/file_validators/validators/file_content_type_validator.rb
+++ lib/file_validators/validators/file_content_type_validator.rb
@@ -10,9 +10,8 @@ module ActiveModel
 
       def validate_each(record, attribute, value)
         unless value.blank?
-          content_type = value.content_type
-          validate_whitelist(record, attribute, content_type)
-          validate_blacklist(record, attribute, content_type)
+          validate_whitelist(record, attribute, value.content_type, option_content_types(record, :allow))
+          validate_blacklist(record, attribute, value.content_type, option_content_types(record, :exclude))
         end
       end
 
@@ -30,21 +29,23 @@ module ActiveModel
 
       private
 
-      def validate_whitelist(record, attribute, value)
-        allowed_types = [options_call(record, :allow)].flatten.compact
-        if allowed_types.present? && allowed_types.none? { |type| type === value }
+      def validate_whitelist(record, attribute, content_type, allowed_types)
+        if allowed_types.present? && allowed_types.none? { |type| type === content_type }
           record.errors.add attribute, :allowed_file_content_types, options.merge(:types => allowed_types.join(', '))
         end
       end
 
-      def validate_blacklist(record, attribute, value)
-        forbidden_types = [options_call(record, :exclude)].flatten.compact
-        if forbidden_types.present? && forbidden_types.any? { |type| type === value }
+      def validate_blacklist(record, attribute, content_type, forbidden_types)
+        if forbidden_types.present? && forbidden_types.any? { |type| type === content_type }
           record.errors.add attribute, :excluded_file_content_types, options.merge(:types => forbidden_types.join(', '))
         end
       end
 
-      def options_call(record, key)
+      def option_content_types(record, key)
+        [get_option_value(record, key)].flatten.compact
+      end
+
+      def get_option_value(record, key)
         options[key].is_a?(Proc) ? options[key].call(record) : options[key]
       end
     end
@@ -52,12 +53,13 @@ module ActiveModel
     module HelperMethods
       # Places ActiveModel validations on the content type of the file
       # assigned. The possible options are:
-      # * +allow+: Allowed content types.  Can be a single content type
-      #   or an array.  Each type can be a String or a Regexp. It should be
-      #   noted that Internet Explorer uploads files with content_types that you
-      #   may not expect. For example, JPEG images are given image/pjpeg and
-      #   PNGs are image/x-png, so keep that in mind when determining how you
-      #   match.  Allows all by default.
+      # * +allow+: Allowed content types. Can be a single content type
+      #   or an array. Each type can be a String or a Regexp. It can also
+      #   be a proc/lambda. It should be noted that Internet Explorer uploads
+      #   files with content_types that you may not expect. For example,
+      #   JPEG images are given image/pjpeg and PNGs are image/x-png, so keep
+      #   that in mind when determining how you match.
+      #   Allows all by default.
       # * +exclude+: Forbidden content types.
       # * +message+: The message to display when the uploaded file has an invalid
       #   content type.
