8e747cba970a7f0b64511ff9497fa5e600d8acb5
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2015-07-31 19:52:44 +0600

    update matchers for expect syntax

--- spec/support/matchers/allow_content_type.rb
+++ spec/support/matchers/allow_content_type.rb
@@ -1,8 +1,8 @@
 RSpec::Matchers.define :allow_file_content_type do |content_type, validator, message|
   match do |model|
     value = double('file', path: content_type, original_filename: content_type)
-    model.any_instance.stub(:read_attribute_for_validation).and_return(value)
-    validator.stub(:detect_content_type).and_return(content_type)
+    allow_any_instance_of(model).to receive(:read_attribute_for_validation).and_return(value)
+    allow(validator).to receive(:detect_content_type).and_return(content_type)
     dummy = model.new
     validator.validate(dummy)
     if message.present?
--- spec/support/matchers/allow_file_size.rb
+++ spec/support/matchers/allow_file_size.rb
@@ -1,7 +1,7 @@
 RSpec::Matchers.define :allow_file_size do |size, validator, message|
   match do |model|
     value = double('file', size: size)
-    model.any_instance.stub(:read_attribute_for_validation).and_return(value)
+    allow_any_instance_of(model).to receive(:read_attribute_for_validation).and_return(value)
     dummy = model.new
     validator.validate(dummy)
     if message.present?
