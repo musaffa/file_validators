b66db8aa20c473e36c0aefd19a30d0bf6e19aac5
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2016-03-31 04:36:42 +0600

    Suppress stdout in the console during test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,11 @@ Coveralls.wear!
 
 locale_path = Dir.glob(File.dirname(__FILE__) + '/locale/*.yml')
 I18n.load_path += locale_path unless I18n.load_path.include?(locale_path)
+I18n.enforce_available_locales = false
 
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
+  # Suppress stdout in the console
+  config.before { allow($stdout).to receive(:write) }
 end
